14655e2bdf063b8766a946fd489ca70ec92ea02e
Fatima Mohamed <adropofilm@gmail.com>
2022-12-12 21:19:59 -0600

    remove nested tasks in rendered json

--- lib/todo_backend_web/views/task_view.ex
+++ lib/todo_backend_web/views/task_view.ex
@@ -3,11 +3,11 @@ defmodule TodoBackendWeb.TaskView do
   alias TodoBackendWeb.TaskView
 
   def render("index.json", %{tasks: tasks}) do
-    %{data: render_many(tasks, TaskView, "task.json")}
+    render_many(tasks, TaskView, "task.json")
   end
 
   def render("show.json", %{task: task}) do
-    %{data: render_one(task, TaskView, "task.json")}
+    render_one(task, TaskView, "task.json")
   end
 
   def render("task.json", %{task: task}) do
